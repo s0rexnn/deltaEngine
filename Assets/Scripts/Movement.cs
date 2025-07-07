d459b7146d3a1ed830a89e292fe35498e2660df4
s0rexnn <ilayplotnikov1@gmail.com>
2025-07-07 23:38:13 +0300

    running

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 5f;
+    public float runningSpeed = 5f;
+    public float maxRunningSpeed = 90f;
+    public float acceleration = 0.0001f;
     private bool isMoving = false;
 
     public Rigidbody2D rb;
@@ -22,6 +25,9 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Debugging // 
+        Debug.Log("Current speed : " + runningSpeed); // Checking speed value
+
         // Disclaimer - This project uses the old input system //
 
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -42,9 +48,24 @@ public class Movement : MonoBehaviour
         {
             isMoving = false;
         }
-            anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isMoving", isMoving);
 
-        if (Mathf.Abs(horizontal) > 0) vertical = 0; // Locks the diagonal movement
+        // Running //
+        if (movement != Vector2.zero)
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+            rb.linearVelocity = new Vector2(horizontal, vertical) * runningSpeed;
+            runningSpeed += acceleration;
+            anim.SetFloat("moveX", horizontal);
+            anim.SetFloat("moveY", vertical);
+            anim.speed = 1.7f;
+        }
+        else
+        {
+            runningSpeed = speed;
+            anim.speed = 1f; 
+        }
+        runningSpeed = Mathf.Clamp(runningSpeed, 0f, maxRunningSpeed); // Clamps the running speed to a maximum value
 
     }
 }
